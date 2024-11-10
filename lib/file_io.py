2a3fcfa8a2f90c6a5deb50972e6dfd16648c525e
Grey Pegram <dpegram85@gmail.com>
2024-11-10 13:04:55 -0500

    Completed Labs

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode="w", encoding='UTF-8') as example_file:
+        example_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode="a", encoding='UTF-8') as example_file:
+        example_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', mode="r", encoding='UTF-8') as example_file:
+        return example_file.read()
